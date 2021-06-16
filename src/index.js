4cbfaa218e0d030fdc635ba246683231e77cbc08
Yanick <tyankee15@gmail.com>
2021-06-16 04:24:14 -0300

    Disable redux-logger in development mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reducers from './reducers';
 import App from './App';
 
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
   <React.StrictMode>
